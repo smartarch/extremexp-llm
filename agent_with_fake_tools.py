86206687584c9aa44860d2dccc2f3c2d0e30f6ec
Michal Töpfer <topfer@d3s.mff.cuni.cz>
2024-03-08 14:01:47 +0100

    Add colorful output to agent_with_fake_tools

--- agent_with_fake_tools.py
+++ agent_with_fake_tools.py
@@ -4,7 +4,7 @@ A LLM-based Agent, all tools are faked and require a human to respond.
 
 from dotenv import load_dotenv, find_dotenv
 from langchain_openai import ChatOpenAI
-# from langchain_community.callbacks import get_openai_callback
+from langchain_community.callbacks import get_openai_callback
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain.agents import AgentExecutor, create_openai_tools_agent
@@ -13,6 +13,12 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.tools import StructuredTool
 import sys
 from datetime import datetime
+from colorama import Fore, Style
+
+load_dotenv(find_dotenv(), override=True)  # take environment variables from .env.
+
+
+########## Helpers ##########
 
 class Logger:
     """Prints the stdout simultaneously to the terminal and a file."""
@@ -29,8 +35,6 @@ class Logger:
 
 sys.stdout = Logger()
 
-print("This script uses multiline input. Press Ctrl+D (Linux) or Ctrl+Z (Windows) on an empty line to end it.")
-
 def multiline_input() -> str:
     result = ""
     while True:
@@ -41,8 +45,27 @@ def multiline_input() -> str:
             break
     return result
 
-load_dotenv(find_dotenv(), override=True)  # take environment variables from .env.
+def print_token_usage(token_counter):
+    return  # this does not work as expected when the LLM is streaming (which it does when run inside agent)
+    print()
+    print(Style.DIM)
+    print(token_counter)
+    print(Style.RESET_ALL)
 
+def print_input_message(message):
+    print("User input:")
+    print(Fore.MAGENTA)
+    print(message)
+    print(Style.RESET_ALL)
+
+def print_result(message):
+    print("Agent reply:")
+    print(Fore.CYAN)
+    print(message)
+    print(Style.RESET_ALL)
+
+
+########## Prompt ##########
 
 MEMORY_KEY = "chat_history"
 
@@ -55,8 +78,13 @@ prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="agent_scratchpad"),
 ])
 
+
+########## LLM ##########
+
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 
+
+########## Tools ##########
 
 # Define the fake tools
 # (later, for real tools, it will be more comfortable to use the @tool decorator, see https://python.langchain.com/docs/modules/agents/tools/custom_tools)
@@ -76,7 +104,8 @@ tools = [
 ]
 
 
-# Create the agent
+########## Agent ##########
+
 agent = create_openai_tools_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=10)  # TODO: max_iterations are for debugging, we can increase it or change to max_execution_time later
 
@@ -91,16 +120,33 @@ agent_with_chat_history = RunnableWithMessageHistory(
 )
 
 
-def chat():
-    message = multiline_input()
-    result = agent_with_chat_history.invoke(
-        {"input": message},
-        # This is needed because in most real world scenarios, a session id is needed
-        # It isn't really used here because we are using a simple in memory ChatMessageHistory
-        config={"configurable": {"session_id": "<foo>"}},
-    )
-    print(result['output'])
+########## Main ##########
+print(f"{Style.DIM}This script uses multiline input. Press Ctrl+D (Linux) or Ctrl+Z (Windows) on an empty line to end input message.{Style.RESET_ALL}\n")
 
+print("Prompt template:\n")
+print(Fore.MAGENTA)
+for message in prompt.messages:
+    print(repr(message))
+print(Style.RESET_ALL)
 
+print("\nStart of chat.\n")
+
+# Chat loop
 while True:
-    chat()
+    message = multiline_input()
+    if message == "":
+        break
+
+    with get_openai_callback() as token_counter:
+        print_input_message(message)
+
+        result = agent_with_chat_history.invoke(
+            {"input": message},
+            # This is needed because in most real world scenarios, a session id is needed
+            # It isn't really used here because we are using a simple in memory ChatMessageHistory
+            config={"configurable": {"session_id": "<foo>"}},
+        )
+
+        print_result(result['output'])
+
+        print_token_usage(token_counter)
